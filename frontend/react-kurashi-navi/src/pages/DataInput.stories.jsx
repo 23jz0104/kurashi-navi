084181e53580f2ccb61d816e4eaf270d39837a63
unknown <3jz@R841PC020.jz.jec.ac.jp>
2025-09-30 15:26:41 +0900

    index.cssの修正とデータ入力用の画面を作成

---
+++ frontend/react-kurashi-navi/src/pages/DataInput.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import DataInput from "./DataInput";
+
+export default {
+  title: 'Pages/DataInput',
+  component: DataInput,
+};
+
+export const Default = () => <DataInput />
