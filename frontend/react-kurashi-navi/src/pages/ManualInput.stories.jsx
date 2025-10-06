bb21f9444e2709af5f0a98ed3fb7923ddf704826
unknown <3jz@R841PC020.jz.jec.ac.jp>
2025-10-06 09:36:36 +0900

    ディレクトリ構成を変更

---
+++ frontend/react-kurashi-navi/src/pages/ManualInput.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ManualInput from "./ManualInput";
+
+export default {
+  title: 'DataInput/ManualInput',
+  component: ManualInput,
+};
+
+export const Default = () => <ManualInput/>
