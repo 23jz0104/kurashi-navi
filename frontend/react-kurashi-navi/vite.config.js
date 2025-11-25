f463adc58cbcd5df03da7554afd08a5085ae3027
23jz0133 <23jz0133@jec.ac.jp>
2025-11-25 13:42:03 +0900

    楽天とヤフーの結合

--- frontend/react-kurashi-navi/vite.config.js
+++ frontend/react-kurashi-navi/vite.config.js
@@ -1,35 +1,38 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// https://vite.dev/config/
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
   server: {
+    // CORS回避用プロキシ
     proxy: {
       '/api': {
-        target: 'https://t08pushtest.mydns.jp',
+        target: 'https://t08.mydns.jp', 
         changeOrigin: true,
         secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '/kakeibo/public/api'),
+        rewrite: (path) => path.replace(/^\/api/, '/kakeibo/public'), 
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(dirname, 'src'),
     }
   },
   test: {
     projects: [{
       extends: true,
       plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
+        storybookTest({
+          configDir: path.join(dirname, '.storybook')
+        })
+      ],
       test: {
         name: 'storybook',
         browser: {
